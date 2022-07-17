16941278eafc4b7d7d4466408dfdf11831c9d0a5
Zafar Iqbal <zafar.amt@gmail.com>
2022-07-17 19:16:51 +0530

    fixed crach issue

--- cmd/ga.c
+++ cmd/ga.c
@@ -8,16 +8,18 @@
 static int do_test_func(struct cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[]) 
 {
     volatile u32 *port1DataReg = (u32*)0x30210000;
-    volatile u32 *port1DirReg  = (u32*)0x30210000;
-    u32 configVal = 0;
+    volatile u32 *port1DirReg  = (u32*)0x30210004;
+    //u32 configVal = 0;
 
     // user led is connected to port 1 6th pin
 
     printf("Testing the customized function\n");
 
-    configVal = PORT_READ(*port1DirReg);
-    configVal = configVal | 0x00000040;
-    PORT_WRITE( *port1DirReg, configVal );
+    //configVal = PORT_READ(*port1DirReg);
+    //configVal = configVal | 0x00000040;
+    //PORT_WRITE( *port1DirReg, configVal );
+
+    *port1DirReg = port1DirReg | 0x00000040;
 
     for(u32 n = 0; n < 10; n++)
     {
