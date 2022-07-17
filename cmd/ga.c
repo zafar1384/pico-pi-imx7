7f6fb2aadf4580813bdc7dfd091b14b9dbf36cab
Zafar Iqbal <zafar.amt@gmail.com>
2022-07-17 18:08:42 +0530

    led blinking got added

--- cmd/ga.c
+++ cmd/ga.c
@@ -1,13 +1,29 @@
 #include <common.h>
 #include <command.h>
-
+#include <linux/delay.h>
 
 
 
 static int do_test_func(struct cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[]) 
 {
-	printf("Testing the customized function\n");
-	return 0;
+    volatile u32 *port1 = (u32*)0x30210000;
+
+    // user led is connected to port 1 6th pin
+
+    printf("Testing the customized function\n");
+
+
+    for(u32 n = 0; n < 10; n++)
+    {
+        printf("Toggling the LED...\n");
+        *port1 = *port1 ^ 0x00000040;
+        mdelay(2000);
+    }
+
+
+
+
+    return 0;
 }
 
 
